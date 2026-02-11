8488f713e15dfc17b6792e748ef979cd3bddf51b
Leonardo Franco <leonardoofranco@outlook.com>
2026-02-11 14:47:31 -0300

    projeto inicial Spring Boot configurado com MySQL

---
+++ src/main/java/com/leonardo/banho_tosa/BanhoTosaApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.banho_tosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanhoTosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanhoTosaApplication.class, args);
+	}
+
+}
